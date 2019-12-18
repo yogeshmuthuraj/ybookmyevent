b1cbaed012ce83a472d5c4145d5be8cbbc316672
Yogesh Muthuraj <Yogesh.Muthuraj@cerner.com>
2019-12-18 12:58:21 +0530

    Addded heroku

--- app/javascript/components/partial/Event.jsx
+++ app/javascript/components/partial/Event.jsx
@@ -2,9 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class Event extends React.Component {
+  openBookTicketsModal() {
+    event.preventDefault();
+
+    $('#bookTicketModal').modal();
+    $(event.target).blur();
+  }
+
   render() {
     const event = this.props.event;
-    const bookEventBtn = <button type="button" className="btn btn-primary btn-block">Book Tickets</button>
+    const BookTicketModal = (
+      <div className="modal fade" id="bookTicketModal" role="dialog">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <h4 className="modal-title">Book tickets</h4>
+            </div>
+            <div className="modal-body">
+              <div className="form-group">
+                <div className="event-image">
+                  <img src={event.image_url} />
+                </div>
+                <div className="event-footer">
+                  <div className="event-details">
+                    <div>{event.name}</div>
+                    <div>{event.description}</div>
+                    <div>{event.event_type}</div>
+                    <div>{event.date}</div>
+                    <div>{event.time}</div>
+                    <div>ticket_price: Rs.{event.ticket_price}/-</div>
+                    <div>Number of persons:
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-success" onClick={(event) => {this.addEvent($(event.target))}}>Confirm</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 
     return (
       <div className="event back-color-white">
@@ -18,10 +58,12 @@ class Event extends React.Component {
             <div>{event.event_type}</div>
             <div>{event.date}</div>
             <div>{event.time}</div>
-            <div>Rs.{event.ticket_price}</div>
+            <div>Rs.{event.ticket_price}/-</div>
           </div>
           <div className="event-status">
-            {bookEventBtn}
+            {BookTicketModal}
+            <button type="button" className="btn btn-primary btn-block" data-target="#bookTicketModal" data-toggle="modal"
+            onClick={(event) => this.openBookTicketsModal(event)}>Book Tickets</button>
           </div>
         </div>
       </div>
